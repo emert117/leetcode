c97b24c9ebacf68810d064e8797d43dbf9c53e1f
Emre MERT <emremert117@gmail.com>
2023-09-26 08:55:48 +0200

    added Remove Duplicates from Sorted Array

---
+++ RemoveDuplicatesFromSorted Array.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int k = 0;
+        for(int i = 1; i < nums.Length;i++){
+            if(nums[k] != nums[i]){
+                nums[k + 1] = nums[i];
+                k++;
+            }
+        }
+        return k + 1;
+    }
