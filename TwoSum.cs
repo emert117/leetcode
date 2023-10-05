81ec1f52c8777810a1000290851757d6a1ca90da
Emre MERT <emremert117@gmail.com>
2023-10-05 15:06:42 +0200

    added Two Sum

---
+++ TwoSum.cs
@@ -0,0 +1,16 @@
+public int[] TwoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+        for (int i=0; i<nums.Length; i++)
+		{
+			for(int j=i+1; j<nums.Length; j++)
+			{
+				if (nums[i] + nums[j] == target)
+				{
+					answer[0] = i;
+					answer[1] = j;
+                    break;
+				}
+			}
+		}
+        return answer;
+    }
