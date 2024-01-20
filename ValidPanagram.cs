d57904ef7f24a91058cd4b5940092608fec0188c
Emre MERT <emremert117@gmail.com>
2024-01-20 18:31:30 +0100

    Valid Panagram

---
+++ ValidPanagram.cs
@@ -0,0 +1,25 @@
+    public bool IsPalindrome(string s) {
+        int start = 0;
+        int end = s.Length - 1;
+        
+        while(start < end){
+            if(!Char.IsLetterOrDigit(s[start])){
+                start++;
+                continue;
+            }
+            
+            if(!Char.IsLetterOrDigit(s[end])){
+                end--;
+                continue;
+            }
+            
+            if(Char.ToLower(s[start]) != Char.ToLower(s[end])){
+                return false;
+            }
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
